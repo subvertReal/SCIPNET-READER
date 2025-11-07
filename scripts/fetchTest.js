da313cf88d4d5d5a993714fba578a32c4315e3ac
subvertReal <122232692+subvertReal@users.noreply.github.com>
2025-11-07 08:19:28 -0500

    Created Repository

---
+++ scripts/fetchTest.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+async function fetchTest() {
+  alert('Simple Button pressed')
+  try {
+    const response = await axios.get('https://scp-wiki.wikidot.com/scp-173');
+    console.log('hi');
+    console.log(response.data);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+  }
+}
+
+export function a(){
+    return fetchTest();
+}
